9e7ed63bc205454583e45a72aede1931a5cb3eb2
MargoFill <fil7marg@gmail.com>
2024-11-15 14:35:46 +0200

    Create load_screen.py
    Same thing, haven't made transitions yet. Need your parts

---
+++ load_screen.py
@@ -0,0 +1,25 @@
+import flet as ft
+
+
+def main(page: ft.Page):
+    page.window_width = 360  
+    page.window_height = 800  
+    page.window_resizable = False  
+    page.bgcolor = "black"
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+
+    image=ft.Image(
+        src=r"C:\Users\Huawei\Documents\first-flet-app\musicappicon.png",
+        width=100,
+        height=100,
+    )
+
+    #page.add(image)
+
+    page.add(ft.Container(
+        content=image,
+        margin=ft.Margin(50, 300, 50, 50),   
+    ))
+
+
+ft.app(main)
